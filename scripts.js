e993c1c449c9971c7c5c73678193534fd1a7f8e4
JAU <javier.aguilaht@gmail.com>
2024-04-01 14:34:33 -0300

    The click event has been created and the test mode of aplication

--- scripts.js
+++ scripts.js
@@ -1,14 +1,39 @@
 (function(){
 
-    const players = [];
+    const gameBoard = {
+        players: [],
+        plays: ['hola','hola'],
 
-    function createPlayer(name, sign){
-        
-         return players.push({name, sign});
+            gameInit: function(){
+                this.cacheDom();
+                this.chooseBox();
+            },
 
-    }
+            cacheDom: function(){
+                this.cardContainer = document.querySelectorAll('.card-container')
+            },
 
+            createPlayer: function(name, sign){  
+                return this.players.push({name, sign})
+            },
+
+            chooseBox: function(){
+                let data = {plays:this.plays}
+                this.cardContainer.forEach(box => {
+                    
+                    box.addEventListener('click', (e) =>{
+                        this.plays.push('x')
+                        box.innerText = data.plays.join(' ');
+                    })
+                    
+                });
+
+            },
+            
+
+
+    };
     
-
+    gameBoard.gameInit();
 
 })();
